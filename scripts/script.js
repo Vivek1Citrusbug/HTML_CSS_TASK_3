6ff59f1dbc008557d65bc232b9983fd2c35efb3e
Vivek Soni <vivek1.citrusbug@gmail.com>
2024-09-23 05:57:17 -0700

    First commit of voyage control

---
+++ scripts/script.js
@@ -0,0 +1,16 @@
+function toggleSidebar() {
+  var sidebar = document.getElementById("mySidebar");
+  var content = document.getElementById("mainContent");
+  var toggleBtn = document.getElementById("sidebarToggle");
+
+  // Toggle the expanded class on the sidebar and content
+  sidebar.classList.toggle("expanded");
+  content.classList.toggle("expanded");
+
+  // Change the button icon based on the sidebar's state
+  // if (sidebar.classList.contains("expanded")) {
+  //     toggleBtn.innerHTML = "×"; // Sidebar is open
+  // } else {
+  //     toggleBtn.innerHTML = "☰"; // Sidebar is closed
+  // }
+}
