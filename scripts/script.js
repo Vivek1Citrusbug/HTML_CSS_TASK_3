a1a7a89080b4c0ccdd9b29dbba8aa8d31b8dd796
Vivek Soni <vivek1.citrusbug@gmail.com>
2024-09-25 00:57:59 -0700

    final commit

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,11 @@
-function toggleSidebar() {
-  var sidebar = document.getElementById("mySidebar");
-  var content = document.getElementById("mainContent");
-  var toggleBtn = document.getElementById("sidebarToggle");
+// function toggleSidebar() {
+//   var sidebar = document.getElementById("mySidebar");
+//   var content = document.getElementById("mainContent");
+//   var toggleBtn = document.getElementById("sidebarToggle");
 
   // Toggle the expanded class on the sidebar and content
-  sidebar.classList.toggle("expanded");
-  content.classList.toggle("expanded");
+  // sidebar.classList.toggle("expanded");
+  // content.classList.toggle("expanded");
 
   // Change the button icon based on the sidebar's state
   // if (sidebar.classList.contains("expanded")) {
@@ -13,4 +13,13 @@ function toggleSidebar() {
   // } else {
   //     toggleBtn.innerHTML = "☰"; // Sidebar is closed
   // }
-}
+// }
+
+const customToggleBtn = document.getElementById('customToggleBtn');
+const customNavbarMenu = document.querySelector('.custom-navbar-menu');
+
+customToggleBtn.addEventListener('click', () => {
+  customNavbarMenu.classList.toggle('active');
+});
+
+
